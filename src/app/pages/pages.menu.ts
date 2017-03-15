098332ae27019489ccb38be718e24d1afffcaf2b
khushi3 <khushboo.bharti@emids.com>
2017-03-15 20:34:36 +0530

    initial commit

---
+++ src/app/pages/pages.menu.ts
@@ -0,0 +1,125 @@
+export const PAGES_MENU = [
+  {
+    path: 'pages',
+    children: [
+      {
+        path: 'dashboard',
+        data: {
+          menu: {
+            title: 'Dashboard',
+            icon: 'ion-android-home',
+            selected: false,
+            expanded: false,
+            order: 0
+          }
+        }
+      },
+      {
+        path: 'editors',
+        data: {
+          menu: {
+            title: 'Editors',
+            icon: 'ion-edit',
+            selected: false,
+            expanded: false,
+            order: 100,
+          }
+        },
+        children: [
+          {
+            path: 'ckeditor',
+            data: {
+              menu: {
+                title: 'CKEditor',
+              }
+            }
+          }
+        ]
+      },
+      {
+        path: 'components',
+        data: {
+          menu: {
+            title: 'Components',
+            icon: 'ion-gear-a',
+            selected: false,
+            expanded: false,
+            order: 250,
+          }
+        },
+        children: [
+          {
+            path: 'treeview',
+            data: {
+              menu: {
+                title: 'Tree View',
+              }
+            }
+          }
+        ]
+      },
+      
+      {
+        path: 'tables',
+        data: {
+          menu: {
+            title: 'Tables',
+            icon: 'ion-grid',
+            selected: false,
+            expanded: false,
+            order: 500,
+          }
+        },
+        children: [
+          {
+            path: 'basictables',
+            data: {
+              menu: {
+                title: 'Basic Tables',
+              }
+            }
+          },
+          {
+            path: 'smarttables',
+            data: {
+              menu: {
+                title: 'Smart Tables',
+              }
+            }
+          }
+        ]
+      },
+     
+      {
+        path: '',
+        data: {
+          menu: {
+            title: 'Pages',
+            icon: 'ion-document',
+            selected: false,
+            expanded: false,
+            order: 650,
+          }
+        },
+        children: [
+          {
+            path: ['/login'],
+            data: {
+              menu: {
+                title: 'Login'
+              }
+            }
+          },
+          {
+            path: ['/register'],
+            data: {
+              menu: {
+                title: 'Register'
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+];
